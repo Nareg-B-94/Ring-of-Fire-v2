4d63008073ce11187706d6e9808e8a9e834b11ff
Nareg Berberian <naregb94@gmail.com>
2022-08-01 08:21:56 +0200

    firebase Deployment test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDlYFTtLVS0InwLKatL49PHgzoQt_-VbiM',
+    authDomain: 'ring-of-fire-3eaeb.firebaseapp.com',
+    projectId: 'ring-of-fire-3eaeb',
+    storageBucket: 'ring-of-fire-3eaeb.appspot.com',
+    messagingSenderId: '932247993673',
+    appId: '1:932247993673:web:f01b3ded25e6f112fc2e98',
+  },
 };
